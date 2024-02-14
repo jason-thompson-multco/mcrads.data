1b9a78aade5b35acaa2b3ba5c23b14ec4031bcf7
Jason Thompson <jthompson12@gmail.com>
2024-02-14 07:31:40 -0800

    Update icd_nchs113causes.R

--- data-raw/icd_nchs113causes.R
+++ data-raw/icd_nchs113causes.R
@@ -8,11 +8,11 @@
   rm(list=ls())
 
 # Function to clean / prep imported data to fix random white spaces (sql_clean) ----
-    source("https://raw.githubusercontent.com/jason-thompson-multco/mcrads/main/R/utilities.R") # pull in rads utilities, particularly sql_clean()
+    source("https://raw.githubusercontent.com/jason-thompson/mcrads/main/R/utilities.R") # pull in rads utilities, particularly sql_clean()
 
 # Function to enumerator all ICD per 113 causes of death (split.dash) ----
     # E.g., split.dash("X0-X04, X17-X19") >> "X00, X01, X02, X03, X04, X17, X18, X19"
-      source("https://raw.githubusercontent.com/jason-thompson-multco/mcrads.data/main/data-raw/icd_utility_split.dash.R")
+      source("https://raw.githubusercontent.com/jason-thompson/mcrads.data/main/data-raw/icd_utility_split.dash.R")
 
 # Read in raw NCHS 113 Selected Causes of Death ICD codes ----
     # Based on pp. 15-17, https://www.cdc.gov/nchs/data/dvs/Part9InstructionManual2020-508.pdf
